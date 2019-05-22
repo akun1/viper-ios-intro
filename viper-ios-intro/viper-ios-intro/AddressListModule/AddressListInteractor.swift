6a19605f43c30ecec741da54df596111f9a40e53
Akash Kundu <akashkundu92@gmail.com>
2019-05-22 10:49:28 -0700

    basic ciper module files made

---
+++ viper-ios-intro/viper-ios-intro/AddressListModule/AddressListInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  AddressListInteractor.swift
+//  viper-ios-intro
+//
+//  Created by Akash Kundu on 5/22/19.
+//  Copyright © 2019 Akash Kundu. All rights reserved.
+//
+
+import Foundation
---
+++ viper-ios-intro/viper-ios-intro/AddressListModule/AddressListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddressListViewController.swift
+//  viper-ios-intro
+//
+//  Created by Akash Kundu on 5/22/19.
+//  Copyright © 2019 Akash Kundu. All rights reserved.
+//
+
+import Foundation
